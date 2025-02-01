4100caaa19a26c78aa6f52272daf45914a145e6a
Atirek Pothiwala <atirekpothiwala@gmail.com>
2025-02-01 23:06:57 +0530

    Initial Commit

---
+++ wheather/wheatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheatherApp.swift
+//  wheather
+//
+//  Created by Atirek Pothiwala on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct wheatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
